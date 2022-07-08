b9b3314d62e98fae4fdba3dd54d4db2a1e235710
Josip Igrec <josip@igr.ec>
2022-07-08 10:00:22 +0200

    Add helper class to add events to entities

---
+++ app/Meta/Eventable.ts
@@ -0,0 +1,19 @@
+export abstract class Eventable<EventMap extends Record<string, (...args: any[]) => void>> {
+  private handlers: Map<keyof EventMap, EventMap[keyof EventMap]>;
+
+  protected constructor() {
+    this.handlers = new Map();
+  }
+
+  public on<Event extends keyof EventMap>(event: Event, handler: EventMap[Event]): this {
+    this.handlers.set(event, handler);
+
+    return this;
+  }
+
+  protected async publish<Event extends keyof EventMap, Handler extends EventMap[Event]>(event: Event, ...args: Parameters<Handler>) {
+    if (this.handlers.has(event)) {
+      await (this.handlers.get(event) as Handler)(...args);
+    }
+  }
+}
