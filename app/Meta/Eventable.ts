54f128fcc28c8c900bfcba24bcd20a43cb45de30
Josip Igrec <josip@igr.ec>
2022-07-12 16:23:15 +0200

    Add ability for Eventable to hold multiple handlers for event

--- app/Meta/Eventable.ts
+++ app/Meta/Eventable.ts
@@ -1,19 +1,27 @@
 export abstract class Eventable<EventMap extends Record<string, (...args: any[]) => any>> {
-  private handlers: Map<keyof EventMap, EventMap[keyof EventMap]>;
+  private handlers: Map<keyof EventMap, EventMap[keyof EventMap][]>;
 
   protected constructor() {
     this.handlers = new Map();
   }
 
   public on<Event extends keyof EventMap>(event: Event, handler: EventMap[Event]): this {
-    this.handlers.set(event, handler);
+    if (!this.handlers.has(event)) {
+      this.handlers.set(event, []);
+    }
+
+    this.handlers.get(event)!.push(handler);
 
     return this;
   }
 
   protected async publish<Event extends keyof EventMap, Handler extends EventMap[Event]>(event: Event, ...args: Parameters<Handler>) {
-    if (this.handlers.has(event)) {
-      await (this.handlers.get(event) as Handler)(...args);
+    if (!this.handlers.has(event)) {
+      return;
+    }
+
+    for (const handler of this.handlers.get(event)!) {
+      await handler(...args);
     }
   }
 }
